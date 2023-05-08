8b4d748e1b22d2e104b4ca982314468bc5848ab4
JBelgrave <JudahBelgrave@gmail.com>
2023-05-07 22:47:20 -0400

    added more enemies, removed fps, enemies shoot at player

--- game-project/script.js
+++ game-project/script.js
@@ -1,38 +1,35 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
+const ammo = [];
+const enemyAmmo = [];
+const enemyQueue = [];
 
 function getRotationTo(a, b) {
     return Math.atan2(a.y - b.y, b.x - a.x);
 };
-
-function isColliding(a, b) {
-    return a.x < b.x + b.w &&
-        a.x + a.w > b.x &&
-        a.y < b.y + b.h &&
-        a.h + a.y > b.y
-};
-
 function drawEnemyElement(obj) {
     ctx.fillStyle = obj.color;
-    ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
+    ctx.fillRect(obj.x, obj.y, obj.size, obj.size);
 };
-
 function drawPlayerElement(obj) {
     ctx.fillStyle = obj.color;
     ctx.beginPath();
     ctx.arc(obj.x, obj.y, obj.radius, obj.startAngle, obj.endAngle, obj.counterclockwise);
     ctx.fill();
 };
-
 function clearScreen() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
+function rngMachine(variable) {
+    return Math.floor(Math.random() * variable)
+};
+function spawnLogic(obj, arr) {
+    if(Math.random() >= 0.5) {
+        arr.push(obj)
+    }
+};
 
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
 
-const ammo = [];
 const mouse = {
     x: 0,
     y: 0,
@@ -42,8 +39,8 @@ const mouse = {
     counterclockwise: false,
     color: 'blue'
 };
-
 let player = {
+    hp: 20,
     x: 500,
     y: 900,
     radius: 15,
@@ -51,46 +48,312 @@ let player = {
     endAngle: 2 * Math.PI,
     counterclockwise: false,
     color: 'red',
-    speed: 7.5,
+    speed: 2.5,
     canShoot: true,
-    onEnterFrame() {
-        if (player.up) {
+    onSpawn() {
+        if (player.up && player.y > 30) {
             player.y -= player.speed
         }
-        if (player.left) {
+        if (player.left && player.x > 30) {
             player.x -= player.speed
         }
-        if (player.right) {
+        if (player.right && player.x < 970) {
             player.x += player.speed
         }
-        if (player.down) {
+        if (player.down && player.y < 970) {
             player.y += player.speed
         }
     }
 };
-
-class Shot {
+class PlayerShotMid {
     constructor() {
-        this.color = 'darkred';
+        this.color = 'yellow';
         this.x = player.x;
         this.y = player.y;
-        this.radius = 10,
-        this.startAngle = 0,
-        this.endAngle = 2 * Math.PI,
-        this.counterclockwise = false,
-        this.speed = 15;
+        this.radius = 10;
+        this.startAngle = 0;
+        this.endAngle = 2 * Math.PI;
+        this.counterclockwise = false;
+        this.speed = 5;
+        this.damage = 1
         this.rotation = getRotationTo(player, mouse);
         ammo.push(this);
-    }
-    onEnterFrame() {
+    };
+    onSpawn() {
         this.x += this.speed * Math.cos(this.rotation);
         this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+class PlayerShotRight {
+    constructor() {
+        this.color = 'yellow';
+        this.x = player.x+10;
+        this.y = player.y;
+        this.radius = 10;
+        this.startAngle = 0;
+        this.endAngle = 2 * Math.PI;
+        this.counterclockwise = false;
+        this.speed = 5;
+        this.damage = 1
+        this.rotation = getRotationTo(player, mouse);
+        ammo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation)+.3;
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+class PlayerShotLeft {
+    constructor() {
+        this.color = 'yellow';
+        this.x = player.x-10;
+        this.y = player.y;
+        this.radius = 10;
+        this.startAngle = 0;
+        this.endAngle = 2 * Math.PI;
+        this.counterclockwise = false;
+        this.speed = 5;
+        this.damage = 1
+        this.rotation = getRotationTo(player, mouse);
+        ammo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation)-.3;
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+
+let enemyA = {
+    hp: 15,
+    x: rngMachine(950),
+    y: rngMachine(950),
+    size: 30,
+    speed: .5,
+    color: 'blue',
+    canShoot: null,
+    movement() {
+        if (this.y < player.y) {
+            this.y += this.speed
+        }
+        if (this.y > player.y) {
+            this.y -= this.speed
+        }
+        if (this.x < player.x) {
+            this.x += this.speed
+        }
+        if (this.x > player.x) {
+            this.x -= this.speed
+        }
+    },
+    collision() {
+        if (this.y > 30) {
+            this.y -= this.speed
+        }
+        if (this.x > 30) {
+            this.x -= this.speed
+        }
+        if (this.x < 970) {
+            this.x += this.speed
+        }
+        if (this.y < 970) {
+            this.y += this.speed
+        }
+    }
+};
+let enemyB = {
+    hp: 20,
+    x: rngMachine(950),
+    y: rngMachine(950),
+    size: 30,
+    speed: .9,
+    color: 'pink',
+    canShoot: null,
+    movement() {
+        if (this.y < player.y) {
+            this.y += this.speed
+        }
+        if (this.y > player.y) {
+            this.y -= this.speed
+        }
+        if (this.x < player.x) {
+            this.x += this.speed
+        }
+        if (this.x > player.x) {
+            this.x -= this.speed
+        }
+    },
+    collision() {
+        if (this.y > 30) {
+            this.y -= this.speed
+        }
+        if (this.x > 30) {
+            this.x -= this.speed
+        }
+        if (this.x < 970) {
+            this.x += this.speed
+        }
+        if (this.y < 970) {
+            this.y += this.speed
+        }
+    }
+};
+let enemyC = {
+    hp: 10,
+    x: rngMachine(950),
+    y: rngMachine(950),
+    size: 30,
+    speed: 1.5,
+    color: 'orange',
+    canShoot: null,
+    movement() {
+        if (this.y < player.y) {
+            this.y += this.speed
+        }
+        if (this.y > player.y) {
+            this.y -= this.speed
+        }
+        if (this.x < player.x) {
+            this.x += this.speed
+        }
+        if (this.x > player.x) {
+            this.x -= this.speed
+        }
+    },
+    collision() {
+        if (this.y > 30) {
+            this.y -= this.speed
+        }
+        if (this.x > 30) {
+            this.x -= this.speed
+        }
+        if (this.x < 970) {
+            this.x += this.speed
+        }
+        if (this.y < 970) {
+            this.y += this.speed
+        }
+    }
+};
+let boss = {
+    hp: 50,
+    x: rngMachine(950),
+    y: rngMachine(950),
+    size: 100,
+    speed: 1,
+    color: 'maroon',
+    canShoot: null,
+    movement() {
+        if (this.y < player.y) {
+            this.y += this.speed
+        }
+        if (this.y > player.y) {
+            this.y -= this.speed
+        }
+        if (this.x < player.x) {
+            this.x += this.speed
+        }
+        if (this.x > player.x) {
+            this.x -= this.speed
+        }
+    },
+    collision() {
+        if (this.y > 30) {
+            this.y -= this.speed
+        }
+        if (this.x > 30) {
+            this.x -= this.speed
+        }
+        if (this.x < 970) {
+            this.x += this.speed
+        }
+        if (this.y < 970) {
+            this.y += this.speed
+        }
     }
 };
 
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
+spawnLogic(enemyA, enemyQueue)
+spawnLogic(enemyB, enemyQueue)
+spawnLogic(enemyC, enemyQueue)
+spawnLogic(boss, enemyQueue)
+
+if(enemyQueue.length === 0) {
+    enemyQueue.push(boss)
+}
+enemyQueue.forEach(enemy => {
+    if(enemyQueue.includes(enemy)) {
+        enemy.canShoot = true
+    } else {
+        enemy.canShoot = false
+    }
+})
+
+
+class EnemyShotA {
+    constructor() {
+        this.color = 'blue';
+        this.x = enemyA.x + 10;
+        this.y = enemyA.y + 5;
+        this.size = 20;
+        this.speed = 7;
+        this.damage = 1;
+        this.rotation = getRotationTo(this, player);
+        enemyAmmo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation);
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+class EnemyShotB {
+    constructor() {
+        this.color = 'pink';
+        this.x = enemyB.x + 10;
+        this.y = enemyB.y + 5;
+        this.size = 20
+        this.speed = 4;
+        this.damage = 3;
+        this.rotation = getRotationTo(this, player);
+        enemyAmmo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation);
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+class EnemyShotC {
+    constructor() {
+        this.color = 'orange';
+        this.x = enemyC.x + 10;
+        this.y = enemyC.y + 5;
+        this.size = 20
+        this.speed = 7;
+        this.damage = 2.5;
+        this.rotation = getRotationTo(this, player);
+        enemyAmmo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation);
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+class BossShot {
+    constructor() {
+        this.color = 'maroon';
+        this.x = boss.x + 10;
+        this.y = boss.y + 5;
+        this.size = 50
+        this.speed = 7;
+        this.damage = 5;
+        this.rotation = getRotationTo(this, player);
+        enemyAmmo.push(this);
+    };
+    onSpawn() {
+        this.x += this.speed * Math.cos(this.rotation);
+        this.y -= this.speed * Math.sin(this.rotation);
+    };
+};
+
 
 window.addEventListener('keydown', e => {
     if (e.key === 'ArrowLeft' || e.key === 'a') {
@@ -103,7 +366,6 @@ window.addEventListener('keydown', e => {
         player.down = true;
     }
 });
-
 window.addEventListener('keyup', e => {
     if (e.key === 'ArrowLeft' || e.key === 'a') {
         player.left = false;
@@ -115,7 +377,6 @@ window.addEventListener('keyup', e => {
         player.down = false;
     }
 });
-
 canvas.addEventListener('mousemove', e => {
     mouse.x = e.offsetX;
     mouse.y = e.offsetY;
@@ -127,26 +388,53 @@ canvas.addEventListener('mouseup', e => {
     player.clicking = false;
 });
 
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------------
 
-const FPS = 60;
 function draw() {
     clearScreen();
+    enemyAmmo.forEach(obj => {
+        drawEnemyElement(obj);
+        obj.onSpawn();
+    });
+    enemyQueue.forEach(obj => {
+        drawEnemyElement(obj);
+        obj.movement();
+        obj.collision();
+    });
     ammo.forEach(obj => {
         drawPlayerElement(obj);
-        obj.onEnterFrame();
+        obj.onSpawn();
     });
-
     drawPlayerElement(player);
-    player.onEnterFrame();
-    //drawPlayerElement(mouse);
-    if (player.clicking && player.canShoot) {
-        new Shot;
-        player.canShoot = false;
-        setTimeout(() => player.canShoot = true, 100);
+    player.onSpawn();
+
+    if (enemyA.canShoot) {
+        new EnemyShotA;
+        enemyA.canShoot = false;
+        setTimeout(() => enemyA.canShoot = true, 1200);
     }
+    if (enemyB.canShoot) {
+        new EnemyShotB;
+        enemyB.canShoot = false;
+        setTimeout(() => enemyB.canShoot = true, 850);
+    }
+    if (enemyC.canShoot) {
+        new EnemyShotC;
+        enemyC.canShoot = false;
+        setTimeout(() => enemyC.canShoot = true, 350);
+    }
+    if (boss.canShoot) {
+        new BossShot;
+        boss.canShoot = false;
+        setTimeout(() => boss.canShoot = true, 500);
+    }
+    
+    if (player.clicking && player.canShoot) {
+        new PlayerShotMid;
+        new PlayerShotRight;
+        new PlayerShotLeft;
+        player.canShoot = false;
+        setTimeout(() => player.canShoot = true, 250);
+    }
+    requestAnimationFrame(draw);
 };
 draw();
-let gameLoop = setInterval(draw, 1000 / FPS);
